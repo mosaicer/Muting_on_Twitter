ecd74af48c9cf30405cf70fabec87825536f4f4f
Cyanoa <scarlet_clover804@yahoo.co.jp>
2019-07-25 23:04:13 +0900

    Add the functions about muting

--- Muting_on_Twitter.user.js
+++ Muting_on_Twitter.user.js
@@ -15,6 +15,19 @@
       return node.querySelector('[aria-label="タイムライン: ホームタイムライン"]');
     };
 
+  const muteIfNeed = function tryToHideTweetNode(node) {
+    const tweetNode = node.querySelector('[data-testid="tweet"]');
+    console.log(tweetNode);
+
+    if (tweetNode && needToMute(tweetNode)) {
+      node.style.display = 'none';
+    }
+  };
+
+  const needToMute = function checkIfTweetNodeNeedsTobeMuted(tweetNode) {
+    return false;
+  };
+
   // observe events that nodes are added in/removed from the root node or its subtree
   const rootNode = document.getElementById('react-root');
   new MutationObserver(mutations =>
