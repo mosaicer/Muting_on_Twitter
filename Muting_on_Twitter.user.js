347ea13692bc30767c2bcb0da9aa3e53ff5b5852
Cyanoa <scarlet_clover804@yahoo.co.jp>
2019-07-25 22:53:28 +0900

    Checked if the added node is the container of tweet nodes

--- Muting_on_Twitter.user.js
+++ Muting_on_Twitter.user.js
@@ -10,12 +10,19 @@
 (() => {
   'use strict';
 
+  const isContainerOfTweetNodes =
+    function checkIfTheNodeContainsParentNodeOfTweetNodes(node) {
+      return node.querySelector('[aria-label="タイムライン: ホームタイムライン"]');
+    };
+
   // observe events that nodes are added in/removed from the root node or its subtree
   const rootNode = document.getElementById('react-root');
   new MutationObserver(mutations =>
     mutations.forEach(mutation =>
       mutation.addedNodes.forEach(node => {
-        console.log(node);
+        if (isContainerOfTweetNodes(node)) {
+          console.log(node);
+        }
       })
     )
   ).observe(rootNode, { childList: true, subtree: true });
